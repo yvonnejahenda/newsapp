d7178ea7b20c746afe74c11b362c05af0b8cc640
Student <yvonnejahendah@gmail.com>
2018-07-18 08:49:45 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+  <div v-for="article in news">
+<img :src="article.urlToImage" alt="">
+<h3>{{article.title}}</h3>
+<h4>{{article.author}}</h4>
+<p>{{article.description}}</p>
+<blockquote>
+<a href="#"target="_blank" class="button">Read more</a>
+</blockquote>
+
+</div>
+    
+  </div>
+</template>
+
+<script>
+const url ="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4ac0fe61c8e44dd3a091a5edcdb93e17"; 
+import axios from 'axios'
+const data = {
+    news: []
+}
+export default {
+  data: function() {
+    return data
+  },
+  created() {
+this.loadnews();
+  },
+  methods:{
+  loadnews(){
+  axios.get(url).then(function(response) {
+  console.log(response.data.articles)
+  data.news = response.data.articles
+  }).catch(function(error)
+  {
+  console.log(error.message)
+  })
+  }
+  }
+
+}
+</script>
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App.vue'
+import 'milligram/dist/milligram.css'
+import './app.css'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
